e7a799d4b607d861c9b24fbbed77e4f99c4990d1
verbosemason <242506479+verbosemason@users.noreply.github.com>
2025-11-19 19:18:43 -0300

    SICP ch 1 ex 1.7

---
+++ sicp/chapter-1/ex-1.7.scm
@@ -0,0 +1,5 @@
+(define (improve guess x) (average guess (/ x guess)))
+(define (average x y) (/ (+ x y) 2))
+(define (good-enough? guess prev-guess) (< (abs (- guess prev-guess)) 0.001))
+(define (sqrt-iter prev-guess guess x) (if (good-enough? guess prev-guess) guess (sqrt-iter guess (improve guess x) x)))
+(define (sqrt x) (sqrt-iter 0 1.0 x))
