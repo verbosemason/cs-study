7dc4d986efa99f37d23afcf39025d3ed1669227c
verbosemason <242506479+verbosemason@users.noreply.github.com>
2025-11-30 00:49:53 -0300

    SICP ch 1 ex 1.40

---
+++ sicp/chapter-1/ex-1.40.scm
@@ -0,0 +1,25 @@
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+     (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+            next
+            (try next))))
+  (try first-guess))
+
+(define (deriv g)
+  (let ((dx 0.00001))
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx))))
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+  
+(define (cubic a b c) (lambda (x) (+ (* x x x) (* a x x) (* b x) c)))
